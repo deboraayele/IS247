24391f89e3e930f791e33dd7c94cd33fa075539a
deboraayele <dayele2@umbc.edu>
2024-07-01 13:12:08 -0400

    deboras changes

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Hotel hotel = new Hotel ("Grand Hotel");
+
+        while (true){
+            System.out.println("1. Book Room");
+            System.out.println("2. Cancel Reservation");
+            System.out.println("3. View Booking History");
+            System.out.println("4. Exit");
+
+            //Check if input is an integer
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid choice. Please try again.");
+                scanner.next(); // Clear invalid input
+                System.out.print("Choose an option: ");
+            }
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    hotel.bookRoom(scanner);
+                    break;
+                case 2:
+                    hotel.cancelReservation();
+                    break;
+                case 3:
+                    hotel.viewBookingHistory();
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the Hotel Reservation System.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+}
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,42 @@
 public class Room {
+    private int roomNumber;
+
+    private String type;
+
+    private double rate;
+
+    private boolean isAvailable;
+
+    public Room(int roomNumber, String type, double rate){
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.rate = rate;
+        this.isAvailable = true;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public boolean isAvailable(){
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available){
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString {
+        return "Room Number: " + roomNumber + ", Type: " + type + ", Rate: " + rate + ", Avaliable" + isAvailable;
+    }
+
 }
